c7ab3e4590aba95244498707038643a8ea059484
Cyrill Marti S Kantonsschule Glarus <cyrill.marti@stud.schulegl.ch>
2024-02-22 10:29:28 +0100

    Added actual experiment code and an Excel document to document

---
+++ motor_pwm_experiment/motor_pwm_experiment.ino
@@ -0,0 +1,22 @@
+#include <Adafruit_MotorShield.h>
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+Adafruit_DCMotor *experimentMotor = AFMS.getMotor(1);
+
+const uint8_t motorSpeed = 24;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Experiment begin \n");
+  AFMS.begin(1600);
+
+  experimentMotor->setSpeed(motorSpeed);
+  experimentMotor->run(FORWARD);
+  experimentMotor->run(RELEASE);
+}
+
+void loop() {
+  experimentMotor->setSpeed(motorSpeed);
+  experimentMotor->run(FORWARD);
+}
